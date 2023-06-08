187f21f62579a0852d55c602029afd00ff7de6b6
MAraya <marioaraya1999@outlook.com>
2023-06-07 18:27:59 -0600

    [add] Arduino Finalizado

--- arduino/Proyecto3_Arduino.ino
+++ arduino/Proyecto3_Arduino.ino
@@ -13,6 +13,13 @@ Servo servoX; //90 Center
 String inputString = "";   // Variable para almacenar la cadena de entrada
 boolean stringComplete = false;  // Bandera que indica si se ha completado la cadena de entrada
 
+int posX = 105;
+int posY = 132; // 130 for medium
+int posZ = 115;
+
+int sepX = 20;
+int sepYZ = 10;
+
 
 
 void setup() {
@@ -23,6 +30,11 @@ void setup() {
 
   reset();
   Serial.begin(9600);
+  moveServo(servoY, 90, 5);
+  delay(100);
+  moveServo(servoX, 80, 5);
+  delay(100);
+  cases(500);
 
 
 }
@@ -59,39 +71,68 @@ void processInputString() {
 void cases(int caso) {
   switch (caso) {
     case 1:
-      press(105, 135, 115);
+      press(posX, posY, posZ);
       break;
     case 2:
-      press(85, 135, 115);
+      press(posX - sepX, posY, posZ);
       break;
     case 3:
-      press(65, 135, 115);
+      press(posX - 2 * sepX, posY, posZ);
       break;
     case 4:
-      press(105, 125, 105);
+      press(posX, posY - sepYZ, posZ - sepYZ);
       break;
     case 5:
-      press(85, 120, 105);
+      press(posX - sepX, posY - sepYZ, posZ - sepYZ);
       break;
     case 6:
-      press(65, 125, 105);
+      press(posX - 2 * sepX, posY - sepYZ, posZ - sepYZ);
       break;
     case 7:
-      press(105, 115, 100);
+      press(posX, posY - 2 * sepYZ, posZ - sepYZ );
       break;
     case 8:
-      press(85, 115, 105);
+      press(posX - sepX, posY - 2 * sepYZ, posZ - sepYZ);
       break;
     case 9:
-      press(65, 115, 105);
+      press(posX - 2 * sepX, posY - 2 * sepYZ, posZ - sepYZ);
       break;
     case 0:
-      press(85, 105, 95);
+      press(posX - sepX, posY - 3 * sepYZ - 5, posZ - 2*sepYZ);
       break;
-    case 10:
+    case 10: //Ok button
       press(65, 90, 100);
+      delay(200);
+      moveServo(servoY, 90, 5);
+      delay(200);
+      moveServo(servoX, 80, 5);
+      delay(200);
       break;
+    case 500://Large Screen
+
+      posX = 105;
+      posY = 130;
+      posZ = 115;
+
+      sepX = 20;
+      sepYZ = 9;
+      break;
+    case 501: // Medium Screen
+
+      posX = 105;
+      posY = 127;
+      posZ = 112;
+
+      sepX = 18;
+      sepYZ = 9;
+      break;
+    case 502: // Small Screen
+      posY = 120;
+      posZ = 107;
+      sepYZ = 7;
+      sepX = 17;
   }
+
 }
 
 void serialEvent() {
@@ -112,25 +153,25 @@ void reset() {
   delay(200);
   moveServo(servoZ, 180, 5);
   delay(200);
-  moveServo(servoY, 90, 5);
-  delay(200);
-  moveServo(servoX, 80, 5);
-  delay(200);
+  /*moveServo(servoY, 90, 5);
+    delay(200);
+    moveServo(servoX, 80, 5);
+    delay(200);*/
 }
 
 void press(int x, int y, int z) {
   moveServo(servoX, x, 10);
-  delay(1000);
+  delay(200);
   moveServo(servoY, y, 10);
-  delay(500);
-  moveServo(servoZ, z, 10);
-
+  delay(200);
+  moveServo(servoZ, z, 15);
   delay(100);
   reset();
 }
 
 void moveServo(Servo servo, int target, int velocity) {
   int currentPos = servo.read();
+
   if (currentPos < target) {
     for (int pos = servo.read(); pos <= target; pos++) {
       servo.write(pos);
